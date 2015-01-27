c0d8893105c10c553040716fe630a99fee63d0c1
Ryan Hays <hays.ryan@gmail.com>
2015-01-27 09:56:05 -0500

    Updated URLs for the files.

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     author=goog_dns.__author__,
     author_email=goog_dns.__author_email__,
     license='gpl-3.0.txt',
-    url='https://github.com/MrJester/',
-    download_url='https://github.com/MrJester/python-dumbpig/tarball/0.0.1',
+    url='https://github.com/MrJester/goog_dns',
+    download_url='https://github.com/MrJester/goog_dns/archive/0.0.1.tar.gz',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 2",
